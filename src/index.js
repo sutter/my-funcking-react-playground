c3ba94022b8bde652ce8dcf6ea0db9a19bf3d88b
Laurent Sutterlity <laurent@macbook-pro-de-laurent.home>
2017-09-07 10:03:49 +0200

    switch to relative path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/pages/index.jsx";
-import registerServiceWorker from "registerServiceWorker";
+import registerServiceWorker from "./registerServiceWorker";
 import "./styles/global";
 
 ReactDOM.render(<App />, document.getElementById("root"));
